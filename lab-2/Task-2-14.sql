4fc1ee83bf3de5f69402f558619ebc657c1796c5
KOKraynov <k.o.kraynov@gmail.com>
2023-11-08 04:19:16 +0400

    Сделал мин и макс в 1 чтение

--- lab-2/Task-2-14.sql
+++ lab-2/Task-2-14.sql
@@ -1,6 +1,9 @@
 -- Task-2-14: Выберите самый дорогой и самый дешевый объект.
 USE cd;
 SELECT facility, initialoutlay
-  FROM facilities
-  WHERE initialoutlay IN (SELECT min(initialoutlay) FROM facilities)
-	OR initialoutlay IN (SELECT max(initialoutlay) FROM facilities);
+  FROM facilities f, 
+       (SELECT min(initialoutlay) as mn,
+               max(initialoutlay) as mx
+          FROM facilities) as mm
+  WHERE initialoutlay = mn
+    or initialoutlay = mx;
