4d33ca431ea2d380be84bacf7834aee083a2be35
KOKraynov <k.o.kraynov@gmail.com>
2023-11-13 16:47:47 +0400

    Поправи на повтор из таблицы

--- lab-4/Task-4-3.sql
+++ lab-4/Task-4-3.sql
@@ -1,7 +1,11 @@
 use cd;
 -- Task-4-3: Добавьте еще одно spa с такими же характеристиками как в задании 1, сгенерировав новое ID.
 INSERT INTO facilities(FACID, facility, membercost, guestcost, initialoutlay, monthlymaintenance)
-  SELECT MAX(FACID) + 1,'spa', 20, 30,  100000,  800
-    FROM facilities;
+  SELECT (select max(FACID)+1 from facilities) as new_fac_id,  
+         f.facility, f.membercost, f.guestcost, 
+         f.initialoutlay, f.monthlymaintenance
+    FROM facilities f
+    where f.facility = 'spa'
+	limit 1;
 COMMIT;
 -- Эта конструкция может бесконечно добавлять с новыми ID
