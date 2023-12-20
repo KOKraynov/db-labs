c44549604225bc9f5a76629546729c9a6cd25fac
KOKraynov <k.o.kraynov@gmail.com>
2023-12-20 19:04:43 +0400

    Сделал структуру 9

--- lab-9/Task-9-1.sql
+++ lab-9/Task-9-1.sql
@@ -2,8 +2,34 @@ CREATE DATABASE potatos;
 
 use potatos;
 
-mysqlimport --ignore-lines=1 \
-            --fields-terminated-by=, \
-            --local -u root \
-            -p Database \
-             TableName.csv
+CREATE TABLE UNSORT_DATA(
+  COLUMN_000 VARCHAR(10),
+  COLUMN_001 VARCHAR(10),
+  COLUMN_002 VARCHAR(10),
+  COLUMN_003 VARCHAR(10),
+  COLUMN_004 VARCHAR(10),
+  COLUMN_005 VARCHAR(10),
+  COLUMN_006 VARCHAR(10),
+  COLUMN_007 VARCHAR(10),
+  COLUMN_008 VARCHAR(10),
+  COLUMN_009 VARCHAR(10)
+  );
+  
+CREATE TABLE potatos(
+  potato_id int primary key AUTO_INCREMENT,
+  color varchar(100)
+  );
+
+CREATE TABLE waves(
+  wave_id int primary key AUTO_INCREMENT,
+  lyambda float NOT NULL
+  );
+
+CREATE TABLE exp_datas(
+  id int primary key AUTO_INCREMENT,
+  potato_id INT NOT NULL,
+  FOREIGN KEY (potato_id) REFERENCES potatos(potato_id),
+  wave_id INT NOT NULL,
+  FOREIGN KEY (wave_id) REFERENCES waves(wave_id),
+  measurement FLOAT NOT NULL
+  );
